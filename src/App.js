c1ac924f6b4b4d969056266d33c2ea642239e180
KhanSaquib <saquibk894@gmail.com>
2022-10-13 00:24:46 +0530

    fixIssue

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import About from './components/About';
 // import About from './components/About';
 import { useState } from 'react';
 import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
   const [alert, setAlert] = useState(null)
   const showAlert=(message,type)=>{
@@ -88,17 +89,17 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtills" mode={mode} toggleMode={toggleMode} toggleMode1={toggleMode1} toggleMode2={toggleMode2} toggleMode3={toggleMode3} />
+    <Alert alert={alert}/>
     <div className='container my-3' >
-    {/* <Routes>
+    <Routes>
           <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/" element={<TextForm heading ='Enter the text below' mode={mode}/>}/>
-        </Routes> */}
-        <Alert alert={alert}/>
-        <TextForm showAlert={showAlert} heading ='Enter the text below' mode={mode}/>
+          <Route exact path="/" element={<TextForm heading ='Enter the text below' mode={mode} showAlert={showAlert}/>}/>
+        </Routes>
+        {/* <TextForm showAlert={showAlert} heading ='Enter the text below' mode={mode}/> */}
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -98,24 +98,24 @@ export default function TextForm(props) {
         <textarea className="form-control" value={Text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8" placeholder='Enter text here' style={{backgroundColor:props.mode==='dark'?'#343a40':'white',color:props.mode==='dark'?'white':'black',resize:'none'}}></textarea>
         </div>
         <div className='cont'>
-        <button className='btn btn-dark' onClick={handleOnClickU} >Convert to uppercase</button>
-        <button className='btn btn-dark mx-2  butto' onClick={handleOnClickL}>Convert to LowerCase</button>
-        <button className='btn btn-dark mx-2 butto' onClick={handleOnClickClear} >Clear text</button>
-        <button className='btn btn-dark mx-2 butto' onClick={handleCopy} >Copy text</button>
-        <button className='btn btn-dark mx-2 butto' onClick={handleRemoveWhitespace} >Remove WhiteSpace</button>
+        <button disabled={Text.length===0} className='btn btn-dark' onClick={handleOnClickU} >Convert to uppercase</button>
+        <button disabled={Text.length===0} className='btn btn-dark mx-2  butto' onClick={handleOnClickL}>Convert to LowerCase</button>
+        <button disabled={Text.length===0}className='btn btn-dark mx-2 butto' onClick={handleOnClickClear} >Clear text</button>
+        <button disabled={Text.length===0}className='btn btn-dark mx-2 butto' onClick={handleCopy} >Copy text</button>
+        <button disabled={Text.length===0}className='btn btn-dark mx-2 butto' onClick={handleRemoveWhitespace} >Remove WhiteSpace</button>
         {/* <button className='btn btn-warning mx-2 my-3' onClick={toggleMode}>{modeBtn}</button> */}
-        <button className='btn btn-dark mx-2 butto' onClick={handleOnClickExtractNumber} >Extract Number</button>
-        <button className='btn btn-dark mx-2 butto' onClick={handleOnClickExtractMail} >Extract email</button>
+        <button disabled={Text.length===0}className='btn btn-dark mx-2 butto' onClick={handleOnClickExtractNumber} >Extract Number</button>
+        <button disabled={Text.length===0}className='btn btn-dark mx-2 butto' onClick={handleOnClickExtractMail} >Extract email</button>
         </div>
 
     </div>
     <div className="container my-3">
         <h2>Your text summary : </h2>
-        <p>{Text.split(" ").length} words and {Text.length} characters</p>
-        <p>{0.008 * Text.split(" ").length} minutes to read</p>
+        <p>{Text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {Text.length} characters</p>
+        <p>{0.008 * Text.split(" ").filter((element)=>{return element.length!==0}).length} minutes to read</p>
         <h2>Preview : </h2>
         <p id='preview'></p>
-        <p>{Text.length>0?Text:"Enter text above to preview here"}</p>
+        <p>{Text.length>0?Text:"Nothing to p"}</p>
     </div>
 
     </>
